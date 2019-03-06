4ffe9ebf7d2580798bfb31429129d7d062dc9cd7
Alison Reilly <reillyamr@gmail.com>
2019-03-06 02:20:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ end
 
 def self.find_by(some_attribute)
   if some_attribute.class == Integer 
-    sql = "SELECT * from #{self.table_name} WHERE '#{some_attribute}' = #{some_attribute}"
+    sql = "SELECT * from #{self.table_name} WHERE '?' = #{some_attribute}"
   DB[:conn].execute(sql, some_attribute)
 else
   sql = "SELECT * from #{self.table_name} WHERE '#{some_attribute}' = '#{some_attribute}'"
